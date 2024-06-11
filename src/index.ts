ce003ec35fcb62e151213a6ba7d4c1ca283ed472
Jeanderson Machado <jeanderson.ferreira74@gmail.com>
2024-06-10 21:43:18 -0300

    Iniciado os estudos com node

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+import express, { Request, Response } from "express";
+
+const server = express();
+
+server.use(express.json());
+
+server.get("/", (request: Request, response: Response) => {
+    return response.status(200).json({ message: "DioBank API" });
+});
+
+server.post("/user", (request: Request, response: Response) => {
+    const body = request.body;
+    console.log(body);
+    return response.status(201).json({ message: "Usuário criado" });
+});
+
+server.listen(5000, () => console.log("server on update"));
